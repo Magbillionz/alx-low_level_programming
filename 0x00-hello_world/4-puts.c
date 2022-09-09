b7db13608d5e0ad65b33a41994dec143becd72f5
Margaret Uduk <marhjuduk@gmail.com>
2022-09-08 17:11:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the sting with put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        puts("\"Progrramming is like a multilingual puzzle");
+        return (0);
+}
